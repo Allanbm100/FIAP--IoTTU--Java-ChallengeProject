8565fab16d926dd7715fe6a3ce24166083c70628
allanbm100 <allan.bm.100@gmail.com>
2025-09-30 16:03:04 -0300

    Adicionando repositories

---
+++ src/main/java/br/com/fiap/iottu/antenna/AntennaRepository.java
@@ -0,0 +1,6 @@
+package br.com.fiap.iottu.antenna;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AntennaRepository extends JpaRepository<Antenna, Integer> {
+}
---
+++ src/main/java/br/com/fiap/iottu/motorcycle/MotorcycleRepository.java
@@ -0,0 +1,6 @@
+package br.com.fiap.iottu.motorcycle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotorcycleRepository extends JpaRepository<Motorcycle, Integer> {
+}
