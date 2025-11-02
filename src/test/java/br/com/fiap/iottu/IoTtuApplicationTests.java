07f758b4589526308b61ff2bc8d6d7330eb35cc4
CAIO LIANG <RM558868@fiap.com.br>
2025-11-02 04:48:45 +0000

    Mapeando Perfil de teste IoTtuApplicationTests.java

--- src/test/java/br/com/fiap/iottu/IoTtuApplicationTests.java
+++ src/test/java/br/com/fiap/iottu/IoTtuApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.fiap.iottu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class IoTtuApplicationTests {
 
