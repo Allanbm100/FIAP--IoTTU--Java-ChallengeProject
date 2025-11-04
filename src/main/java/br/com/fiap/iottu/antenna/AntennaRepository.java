7812fa9cadc34fb3ae658519ac4eff64bd6aa6bb
allanbm100 <allan.bm.100@gmail.com>
2025-11-03 23:41:27 -0300

    Ajustes nas classes de antena e repositório

--- src/main/java/br/com/fiap/iottu/antenna/AntennaRepository.java
+++ src/main/java/br/com/fiap/iottu/antenna/AntennaRepository.java
@@ -8,6 +8,11 @@ import java.util.Optional;
 public interface AntennaRepository extends JpaRepository<Antenna, Integer> {
 
     List<Antenna> findByYardId(Integer yardId);
+    List<Antenna> findByYardUserId(Integer userId);
     Optional<Antenna> findByCode(String code);
+
+    Optional<Antenna> findByCodeAndYardId(String code, Integer yardId);
+
+    List<Antenna> findAllByLatitudeAndLongitudeAndYardId(java.math.BigDecimal latitude, java.math.BigDecimal longitude, Integer yardId);
 
 }
