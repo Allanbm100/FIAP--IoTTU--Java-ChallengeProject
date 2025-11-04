d69682448230d2bf4fee12e9d6b63ab4a27621f7
allanbm100 <allan.bm.100@gmail.com>
2025-11-03 23:48:33 -0300

    Habilitar caching na aplicação principal e preparar profiles

--- src/main/java/br/com/fiap/iottu/IoTtuApplication.java
+++ src/main/java/br/com/fiap/iottu/IoTtuApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.iottu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class IoTtuApplication {
 
     public static void main(String[] args) {
